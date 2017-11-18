eb5d4795c5a00cfc66da2deaa8fc1348aabf58d5
samtomcom <sam.comer@gmail.com>
2017-11-18 20:53:55 +0000

    updated

--- lib/factorise.rb
+++ lib/factorise.rb
@@ -1,5 +1,28 @@
 require "factorise/version"
+require 'prime'
 
 module Factorise
-  # Your code goes here...
+  def prime_factors
+    number = self.to_s.dup.to_i
+    factors = []
+
+    primes_below = Prime.take_while { |p| p <= number / 2 }
+
+    primes_below.each do |p|
+      until number % p != 0
+        factors << p
+        number = number / p
+      end
+    end
+
+    if factors.length == 0
+      return [self]
+    else
+      return factors
+    end
+  end
+end
+
+class Integer
+  include Factorise
 end
